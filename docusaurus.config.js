97daa435acce5cbd2921ffea6586460873c192db
beniroquai <bene.d@gmx.de>
2024-04-17 00:29:08 +0200

    adding compute results

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,6 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
-
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+import {themes as prismThemes} from 'prism-react-renderer'
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -13,7 +11,9 @@ const config = {
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-
+  plugins: [
+    '@datalayer/jupyter-docusaurus-plugin'
+  ],
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'openSIMMO', // Usually your GitHub org/user name.
@@ -141,8 +141,8 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} openSIMMO Developers.`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
       },
     }),
 };
